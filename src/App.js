f5fb8477b653579c574a07e761dfd074f60c9790
Shifang Zhao <szhao063@uottawa.ca>
2022-09-18 13:35:47 -0400

    My commit

--- src/App.js
+++ src/App.js
@@ -8,36 +8,31 @@ import {BrowserRouter as Router, Route,Routes, Link} from "react-router-dom";
 import Login from './Component/Login';
 import Home from './Component/Home';
 
-var myStyle = {
-  backgroundColor: 'blue',
-  height:'100vh'
-}
-
 function App() {
   return (
     <Router>
-      <div style={myStyle}>
+      <div className='box'>
         <nav>
           <ul>
             <li>
             <div className='head'>
-              <img src={robot} width="150" alt="robot"/>
+              <img className="icon" src={robot} width="150" alt="robot"/>
               <h1 className='title'>Smart Delivery!</h1>
             </div>
               <div className='home'>
-                <img src={home} width="30" alt="home"/>
+                <img className="logo1" src={home} width="30" alt="home"/>
                 <Link className='link1' to="/home">Home</Link>
               </div>
               <div className='intro'>
-                <img src={intro} width="30" alt="intro"/>
+                <img className="logo2" src={intro} width="30" alt="intro"/>
                 <Link className='link2' to="/home">Introduction</Link>
               </div>
               <div className='contributor'>
-                <img src={people} width="30" alt="home"/>
+                <img className="logo3" src={people} width="30" alt="home"/>
                 <Link className='link3' to="/home">Contributors</Link>
               </div>
               <div className='login'>
-                <img src={login} width="30" alt="home"/>
+                <img className="logo4" src={login} width="30" alt="home"/>
                 <Link className='link4' to="/login">Login</Link>
               </div>
             </li>
